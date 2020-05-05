8513662f056820595c46b6579d6a54581aa82237
Drew Baker <drewrb@gmail.com>
2020-05-05 12:03:58 -0700

    Just corrected typo with version number

--- wp-graphql-cors.php
+++ wp-graphql-cors.php
@@ -5,7 +5,7 @@
  * and response cookie header.
  * Text Domain: wp-graphql-cors
  * Domain Path: /languages
- * Version: 1.0
+ * Version: 1.0.0
  *
  * @category WPGraphQL_Extension
  * @package  wp-graphql-cors
