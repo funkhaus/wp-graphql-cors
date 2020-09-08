f7ef401723b7b910b3ebc0a04aa4ebad2bc61735
Jacob Arriola <jacobarriola@users.noreply.github.com>
2020-09-08 06:33:41 -0700

    Remove hanging bracket

--- includes/login-mutation.php
+++ includes/login-mutation.php
@@ -65,7 +65,7 @@ function wpgraphql_cors_login_mutation() {
 					$user = wp_signon( $credentials, true );
 
 					if ( is_wp_error( $user ) ) {
-						throw new UserError( ! empty( $user->get_error_code() ) ? $user->get_error_code() : 'invalid login' ) );
+						throw new UserError( ! empty( $user->get_error_code() ) ? $user->get_error_code() : 'invalid login' );
 					}
 
 					return array( 'status' => 'SUCCESS' );
